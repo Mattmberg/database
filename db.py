ad3bdd4cd509fbd2f689cdd2a9d3253c08d66457
Matthew Bergeron Jr <84132501+Mattmberg@users.noreply.github.com>
2022-01-03 20:57:39 -0500

    Updated to add While statement
    Added While statement- 
    
    'q' to break the loop
    
    'a' to append the database
    
    'r' to read the database

--- db.py
+++ db.py
@@ -1,13 +1,28 @@
-import mysql.connector
-import getpass
-  
+from mysql.connector import connect, Error
+from getpass import getpass
+
+options = ["a", "r", "q"]
+
 try:
-      db = mysql.connector.connect(
-        host="",
+    with connect(
+        host="localhost",
         user=input("Enter username: "),
-        password=getpass("Enter password: ")
-    )
-    
+        password=getpass("Enter password: "),
+    ) as connection:
+        create_db_query = "CREATE DATABASE deck_colors"
+        with connection.cursor() as cursor:
+            cursor.execute(create_db_query)
+        #print(connection)
 except Error as e:
-  print(e)
-        
+    print(e)
+
+while True:
+    user_choice = input("Welcome to the Deck List database, what would you like to do? (a)dd to data, (r)ead the data, or (q)uit the program: ")
+
+    if user_choice == "q":
+        break
+
+    if user_choice == "a":
+
+    if user_choice == "r":
+        cursor.execute("SELECT * FROM deck_colors")
